4e650d44efb1326658ca8b1bda8c9550e1295fc3
Vladyslav Karmazyn <karmazynnnn@gmail.com>
2022-08-30 16:44:34 +0300

    sidebar acordion

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const sidebar = document.querySelector('.sidebar-title');
+const techSkills = document.querySelector('#tech_skill');
 const techSkillList = document.querySelector('.tech-skill-list');
-console.log(sidebar);
+
 console.log(techSkillList);
-sidebar.addEventListener('click', function () {
-    techSkillList.classList.toggle('tech-skill-active')
+techSkills.addEventListener('click', function () {
+    techSkillList.classList.toggle('active_acordion')
 })
 
+const contacts = document.querySelector('#contact_title');
+const contactList = document.querySelector('#contact-list')
+contacts.addEventListener('click', function () {
+    contactList.classList.toggle('active_acordion')
+})
+
+const softSkills = document.querySelector('#soft-skill-list');
+const softSkillList = document.querySelector('.soft-skill-list')
+softSkills.addEventListener('click', function () {
+    softSkillList.classList.toggle('active_acordion')
+})
